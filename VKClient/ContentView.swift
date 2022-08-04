8dae1e71409205fb734072144c43b4fd445a4858
DmitriqPanteleev <pantele3vd@gmail.com>
2022-08-04 16:23:11 +0400

    Initial Commit

---
+++ VKClient/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VKClient
+//
+//  Created by Дмитрий Пантелеев on 04.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VKClient/VKClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  VKClientApp.swift
+//  VKClient
+//
+//  Created by Дмитрий Пантелеев on 04.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct VKClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
